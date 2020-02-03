9367eee737813c433c96575800871aa0500c3658
day4me <nickydragon1999@gmail.com>
2020-02-03 13:11:16 +0000

    update

--- .config/awesome/widgets/todo.lua
+++ .config/awesome/widgets/todo.lua
@@ -11,7 +11,7 @@ local s = awful.screen.focused()
 todo = wibox({width=400, height=400, visible = false, type = "normal", screen = s})
 --awful.placement.maximize(todo)
 
-todo:buttons(gears.table.join(awful.button({}, 1, function () awful.mouse.client.move(todo) end)))
+todo:buttons(gears.table.join(awful.button({}, 1, function () awful.mouse.wibox.move() end)))
 
 function todo_show()
     todo.visible = true
