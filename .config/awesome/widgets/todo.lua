0b5f915fb6d975dc0daeeeeeeb6da21f5e546bc1
day4me <nickydragon1999@gmail.com>
2020-02-04 01:27:34 +0000

    update

--- .config/awesome/widgets/todo.lua
+++ .config/awesome/widgets/todo.lua
@@ -8,10 +8,10 @@ local modkey = "Mod4"
 
 local s = awful.screen.focused()
 
-todo = wibox({width=400, height=400, visible = false, type = "normal", screen = s})
+local todo = wibox({width=400, height=400, visible = false, type = "normal", screen = s})
 --awful.placement.maximize(todo)
 
-todo:buttons(gears.table.join(awful.button({}, 1, function () awful.mouse.wibox.move() end)))
+todo:buttons(gears.table.join(awful.button({modkey}, 1, function () awful.mouse.wibox.move(todo) end)))
 
 function todo_show()
     todo.visible = true
