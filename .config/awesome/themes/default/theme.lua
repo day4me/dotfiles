a532e923a0e928e6516f0cd848ca4cec47288839
day4me <nickydragon1999@gmail.com>
2020-02-10 12:01:07 +0000

    update

--- .config/awesome/themes/default/theme.lua
+++ .config/awesome/themes/default/theme.lua
@@ -7,7 +7,7 @@ local xresources = require("beautiful.xresources")
 local dpi = xresources.apply_dpi
 
 local gfs = require("gears.filesystem")
-local themes_path = "/home/day4me/.config/awesome/themes/"
+local themes_path = os.getenv("HOME") .. "/.config/awesome/themes/"
 
 local theme = {}
 
--- .config/awesome/widgets/todo.lua
+++ .config/awesome/widgets/todo.lua
@@ -11,6 +11,15 @@ local s = awful.screen.focused()
 local todo = wibox({width=400, height=400, visible = false, type = "normal", screen = s})
 --awful.placement.maximize(todo)
 
+local w = wibox.widget{
+    markup = 'This <i>is</i> a <b>textbox</b>!!!',
+    align  = 'center',
+    valign = 'center',
+    widget = wibox.widget.textbox
+}
+
+todo:set_widget(w)
+
 todo:buttons(gears.table.join(awful.button({modkey}, 1, function () awful.mouse.wibox.move(todo) end)))
 
 function todo_show()
